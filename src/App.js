7cb47dfa1296d76fad9cf3fe429e0d8b95565c53
believemaster <believemasters@gmail.com>
2022-06-13 20:20:52 +0530

    Creating component of Counter

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import './App.css';
 import { Component } from 'react';
 import FirstComponent from '../src/components/learning-examples/FirstComponent';
 import SecondComponent, { ThirdComponent } from '../src/components/learning-examples/SecondAndThirdComponent';
+import Counter from './components/counter/Counter';
 
 // React Function Component
 function App() {
   return (
-    <div className="App">
-      This is APP component which is created using a function component
-      <FirstComponent/>
-      <SecondComponent/>
-      <ThirdComponent/>
+    <div className="App">      
+      {/* <LearningComponents/> uncomment if want to test */}
+      <Counter/>
     </div>
   );
+}
+
+class LearningComponents extends Component {
+  render() {
+    return(
+      // Components must be wrappedn inside divs
+      <div className='LearningComponents'>
+        This is APP component which is created using a function component
+        <FirstComponent/>
+        <SecondComponent/>
+        <ThirdComponent/>
+      </div>
+    );
+  }
 }
 
 export default App;
